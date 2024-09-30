a054c9e022af7a647546bb9f4431fc4e867b459a
Kirill_S <kiris2004@mail.ru>
2024-09-30 11:25:59 +0300

    fixed a bug in route article

--- hexlet_django_blog/article/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Article(models.Model):
-    name = models.CharField(max_length=200)
-    body = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
